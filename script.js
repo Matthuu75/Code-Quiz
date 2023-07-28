ea6d1c350c8570abe47a247cbfd8743a6c5e8fd4
Marcus Matthews <mlmatthews75@gmail.com>
2023-07-27 22:31:10 -0400

    added answer selection. correct = green, wrong = red. Added display functions to the question/answers

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// // This is to submit the page (look at activity 05)
-// FormEl. on("submit", function (e) {
-//     e.preventDefault()
-//     console.log('Form Submitted')
-// })
-
 const questions = [
     {
         //question index 0
@@ -11,7 +5,7 @@ const questions = [
         answers: [
             { text: "Yellow", correct: true },
             { text: "Blue", correct: false },
-            { text: "Red", correct: false }
+            { text: "Red", correct: false },
         ]
     },
     {
@@ -20,7 +14,7 @@ const questions = [
         answers: [
             { text: "Mouse", correct: false },
             { text: "Giraffe", correct: true },
-            { text: "Dog", correct: false }
+            { text: "Dog", correct: false },
         ]
     },
     {
@@ -29,18 +23,19 @@ const questions = [
         answers: [
             { text: "Yellow", correct: false },
             { text: "Blue", correct: false },
-            { text: "Green", correct: true }
+            { text: "Green", correct: true },
         ]
     }
 ];
 
 const questionEl = document.getElementById("question");
-const answerBtn = document.getElementById("answer-btn");
+const answerBtns = document.getElementById("answer-btns");
 const nextBtn = document.getElementById("next-btn");
 
 let currentQuestionIndex = 0;
 let score = 0;
 
+//when the quiz is started, reset score and reset questions
 function startQuiz() {
     currentQuestionIndex = 0;
     score = 0;
@@ -50,40 +45,68 @@ function startQuiz() {
 
 function showQuestion() {
     let currentQuestion = questions[currentQuestionIndex];
-    let questionNo = currentQuestion = 1;
-    questionEl.innerHTML = questions + ". " + currentQuestion.question;
+    let questionNum = currentQuestion + 1;
+    questionEl.innerHTML = questionNum + ". " + currentQuestion.question;
 
     currentQuestion.answers.forEach(answer => {
         const button = document.createElement("button");
         button.innerHTML = answer.text;
         button.classList.add("btn");
-        answerBtn.appendChild(button);
+        answerBtns.appendChild(button);
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer);
     })
 }
+
+function resetQuiz() {
+    nextBtn.style.display = "none";
+    while (answerBtns.firstChild) {
+        answerBtns.removeChild(answerBtns.firstChild);
+    }
+}
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if (isCorrect) {
+        selectedBtn.classList.add("correct");
+    } else {
+        selectedBtn.classList.add("incorrect")
+    }
+    Array.from(answerBtns.children).forEach(button => {
+        if (button.dataset.correct === "true") {
+            button.classList.add("corect");
+        }
+        button.disabled = true;
+    })
+    nextBtn.style.display = "block";
+}
+
 startQuiz();
 
-var timerEl = document.getElementById('timer')
-var startQuiz = document.getElementById('#startQuiz')
-var remainingTime = document.getElementById('remainingTime')
-const button = document.querySelector("#button")
+// var timerEl = document.getElementById('timer')
+// var startQuiz = document.getElementById('#startQuiz')
+// var remainingTime = document.getElementById('remainingTime')
+// const button = document.querySelector("#button")
 
-//need to figure out how to get this timer to start on the start quiz button
-addEventListener("click", (e) => {
-    console.log("start timer")
-    countdown()
-});
+// //need to figure out how to get this timer to start on the start quiz button
+// addEventListener("click", (e) => {
+//     console.log("start timer")
+//     countdown()
+// });
 
-function countdown() {
-    // onclick EVENT
-    var timeLeft = 60;
-    var timeInterval = setInterval(function () {
-        if (timeLeft > 1) {
-            remainingTime.textContent = 'Time: ' + timeLeft;
-            timeLeft--;
-        } else {
-            clearInterval(timeInterval);
-            remainingTime.textContent = "Time's Up"
-        }
-    }, 1000);
-}
+// function countdown() {
+//     // onclick EVENT
+//     var timeLeft = 60;
+//     var timeInterval = setInterval(function () {
+//         if (timeLeft > 1) {
+//             remainingTime.textContent = 'Time: ' + timeLeft;
+//             timeLeft--;
+//         } else {
+//             clearInterval(timeInterval);
+//             remainingTime.textContent = "Time's Up"
+//         }
+//     }, 1000);
+// }
 
